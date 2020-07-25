1bf760fbea5a1e685f55b02ca9aaa4e9e6fc7d84
muhamedsuhail <muhamed.suhail1@gmail.com>
2020-07-25 21:33:14 +0530

    Updated DATABASE_URL

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ from login import *
 app = Flask(__name__)
 
 #Check for environment variable
-if not DATABASE_URL:
+if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
@@ -21,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(DATABASE_URL)
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/",methods=["GET","POST"])
